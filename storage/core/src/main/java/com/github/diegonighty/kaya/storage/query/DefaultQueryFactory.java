289bf571b9f5c2875c8caa5db60e66c7297cbe5a
diegonighty <cardenasdiego003@gmail.com>
2022-11-17 21:35:43 -0600

    refactor: QueryFactory now is abstract

---
+++ storage/core/src/main/java/com/github/diegonighty/kaya/storage/query/DefaultQueryFactory.java
@@ -0,0 +1,58 @@
+package com.github.diegonighty.kaya.storage.query;
+
+import com.github.diegonighty.kaya.storage.processor.element.RepositoryMethodElement;
+import com.github.diegonighty.kaya.storage.query.filter.FilterSequence;
+import com.github.diegonighty.kaya.storage.query.io.PrintableQuery;
+
+import java.util.Arrays;
+
+public class DefaultQueryFactory implements QueryFactory {
+
+    //todo: add support for configuration
+    static final String GETTER_PREFIX = "get";
+
+    static final int END_OF_METHOD_DECORATORS = 3;
+    static final int REQUIRED_METHOD_TERMS = 4;
+
+    static final int METHOD_NAME_INDEX = 0;
+    static final int METHOD_RETURN_TYPE_INDEX = 1;
+
+    private final FilterFactory filterFactory;
+    private final DelegatedQueryFactory queryFactory;
+
+    public DefaultQueryFactory(FilterFactory filterFactory, DelegatedQueryFactory delegatedQueryFactory) {
+        this.filterFactory = filterFactory;
+        this.queryFactory = delegatedQueryFactory;
+    }
+
+    @Override
+    public PrintableQuery create(RepositoryMethodElement methodElement) {
+        var methodName = methodElement.name().toString();
+
+        var terms = methodName.split("(?=\\p{Upper})");
+        var termSize = terms.length;
+
+        if (termSize < REQUIRED_METHOD_TERMS) {
+            throw new IllegalArgumentException(
+                    "Invalid method name: " + methodName + ". Expected: <method><quantity>By<filter> : findOneByName"
+            );
+        }
+
+        var startFilter = termSize - END_OF_METHOD_DECORATORS;
+
+        var filter = Arrays.copyOfRange(terms, termSize - startFilter, termSize);
+        var filterSequence = filterFactory.create(filter, GETTER_PREFIX);
+
+        return queryFactory.create(
+                new QueryContext(terms[METHOD_NAME_INDEX], terms[METHOD_RETURN_TYPE_INDEX], filterSequence.build())
+        );
+    }
+
+    public interface FilterFactory {
+        FilterSequence create(String[] filter, String getterPrefix);
+    }
+
+    public interface DelegatedQueryFactory {
+        PrintableQuery create(QueryContext context);
+    }
+}
